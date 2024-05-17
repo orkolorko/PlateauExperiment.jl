e0c989689cc59541866d9cb519273f33d9275449
Documenter.jl <documenter@juliadocs.github.io>
2024-05-17 14:53:01 +0000

    build based on 8962ba2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlateauExperiment","category":"page"},{"location":"#PlateauExperiment","page":"Home","title":"PlateauExperiment","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlateauExperiment.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlateauExperiment]","category":"page"}]
+}
