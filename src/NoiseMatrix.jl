1e0781d58577064c3c2d890e58d25f4211bba6d9
Isaia Nisoli <nisoli@im.ufrj.br>
2024-05-17 15:04:20 -0300

    Added NoiseBall, renamed D to NoiseInterval

--- src/NoiseMatrix.jl
+++ src/NoiseMatrix.jl
@@ -1,9 +1,21 @@
+using IntervalArithmetic, BallArithmetic
 
 """
-Builds a Matrix representig Convolution with Gaussian Noise of variance σ
+Build a Matrix of Intervals representig Convolution with Gaussian Noise of variance σ
 Galerkin truncated at frequence K
 """
-function D(σ, K)
+function NoiseInterval(σ, K)
     return Diagonal([[exp((-σ^2 * π^2 * interval(k)^2) / 2) for k in 0:K];
                      [exp((-σ^2 * π^2 * interval(k)^2) / 2) for k in (-K):-1]])
+end
+
+
+"""
+Build a Matrix of Balls representig Convolution with Gaussian Noise of variance σ
+Galerkin truncated at frequence K
+"""
+function NoiseBall(σ, K)
+    D = NoiseInterval(σ, K)
+    Dc, Dr = IntervalArithmetic.mid.(D), IntervalArithmetic.radius.(D)
+    bD = BallMatrix(Dc, Dr)
 end
