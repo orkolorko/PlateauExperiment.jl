a94760e6982d6341024cf3ea7680bc0f7f693375
Isaia Nisoli <nisoli@im.ufrj.br>
2024-06-07 13:41:39 -0300

    Corrected norms, strange result

--- src/Experiment.jl
+++ src/Experiment.jl
@@ -4,19 +4,45 @@ export Experiment, MultipleExperiments
 function power_norms(A, N)
     norms = zeros(N)
     
+    K = Inf
+
     Aiter = A
     for i in 1:N
         norms[i] = BallArithmetic.svd_bound_L2_opnorm(Aiter)
         Aiter *= A
+        if norms[i]<1
+            K = i
+            break
+        end
     end
-    return norms
+
+    return norms[1:K]
 end
 
 Γ(σ, K) = ((1 / (sqrt(σ^2 * 2 * π)))exp((-σ^2 * K^2 * π^2) / 2))
 
 bound_ρ_σ_2(σ) = sqrt(1 / (sqrt(σ^2 * 2 * π)))
 
-function deterministic_discretized(α, β, K)
+
+# function process_norms(norms)
+#     rest = 0
+#     contract = false
+#     for i in 1:length(norms)
+#         if norms[i] < 1
+#             contract = true
+#         end
+#         if norms[i] > 1 && contract == true
+#             rest = i
+#             break
+#         end
+#     end
+#     good_norms = norms[1:rest]
+#     return good_norms
+# end
+
+
+function Experiment(α, β, σ, K; max_iter = 20)
+    
     FFTNx = 8*K
     B = FourierAdjoint(K, FFTNx) 
     D(x) = T(x; α, β)
@@ -46,7 +72,11 @@ function Experiment(α, β, σ, K;
 
     A = PσK[2:end, 2:end]
     norms = power_norms(A, max_iter)
-    @info "Norms" norms
+    
+    
+    @info "Norms" 
+    @info norms[1]
+    @info norms[end]
 
     valΓ = Γ(σ, K)
     @info "Γ" valΓ
@@ -55,9 +85,11 @@ function Experiment(α, β, σ, K;
 
     coeff_err = ((1+valΓ+valρ)*valΓ+ϵ)
     err_L2 = (sum(norms)*coeff_err)/(1-norms[end])
-    @info err_L2
+    @info "err_L2" err_L2
     valΥ = Υ(α, β)
-    @info valΥ
+    @info "Υ" valΥ
+    @info "diam λ" diam(real(λ))
+
     return real(λ)+valΥ * hull(-err_L2, err_L2)
 end
 
